c925dc7f37ce0ceac6df4fce646ee7ffc978139a
elbse <changpriego@gmail.com>
2025-12-14 13:22:23 +0800

    ahh

--- obj/Debug/net10.0-windows/C#_POS_System.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/C#_POS_System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#_POS_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d44be74777814a1252e1f653e265ca99001da23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eac8070bbb6aac488f144876ace4959e51c41e61")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#_POS_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#_POS_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
